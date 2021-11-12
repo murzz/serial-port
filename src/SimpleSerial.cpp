b9d8c33a6b9f24e0ebe97ddec25304796f50edbc
Eric Fontaine <ericfontainejazz@gmail.com>
2021-11-12 00:14:20 -0500

    removed unnecessary tab

--- src/SimpleSerial.cpp
+++ src/SimpleSerial.cpp
@@ -11,33 +11,32 @@
 
 SimpleSerial::SimpleSerial(std::string port, unsigned int baud_rate)
     : io(), serial(io,port)
-    {
-        serial.set_option(boost::asio::serial_port_base::baud_rate(baud_rate));
-    }
+{
+    serial.set_option(boost::asio::serial_port_base::baud_rate(baud_rate));
+}
 
 void SimpleSerial::writeString(std::string s)
-    {
-        boost::asio::write(serial,boost::asio::buffer(s.c_str(),s.size()));
-    }
+{
+    boost::asio::write(serial,boost::asio::buffer(s.c_str(),s.size()));
+}
 
 std::string SimpleSerial::readLine()
+{
+    //Reading data char by char, code is optimized for simplicity, not speed
+    using namespace boost;
+    char c;
+    std::string result;
+    for(;;)
     {
-        //Reading data char by char, code is optimized for simplicity, not speed
-        using namespace boost;
-        char c;
-        std::string result;
-        for(;;)
+        asio::read(serial,asio::buffer(&c,1));
+        switch(c)
         {
-            asio::read(serial,asio::buffer(&c,1));
-            switch(c)
-            {
-                case '\r':
-                    break;
-                case '\n':
-                    return result;
-                default:
-                    result+=c;
-            }
+        case '\r':
+            break;
+        case '\n':
+            return result;
+        default:
+            result+=c;
         }
     }
-
+}
