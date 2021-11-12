fe2f51c1302ac5a3385fdd5296d708de09df6add
Eric Fontaine <ericfontainejazz@gmail.com>
2021-11-11 23:58:01 -0500

    split SimpleSerial into .cpp and .h

--- include/SimpleSerial.h
+++ include/SimpleSerial.h
@@ -21,21 +21,14 @@ public:
      * \throws boost::system::system_error if cannot open the
      * serial device
      */
-    SimpleSerial(std::string port, unsigned int baud_rate)
-    : io(), serial(io,port)
-    {
-        serial.set_option(boost::asio::serial_port_base::baud_rate(baud_rate));
-    }
+    SimpleSerial(std::string port, unsigned int baud_rate);
 
     /**
      * Write a string to the serial device.
      * \param s string to write
      * \throws boost::system::system_error on failure
      */
-    void writeString(std::string s)
-    {
-        boost::asio::write(serial,boost::asio::buffer(s.c_str(),s.size()));
-    }
+    void writeString(std::string s);
 
     /**
      * Blocks until a line is received from the serial device.
@@ -43,26 +36,7 @@ public:
      * \return a string containing the received line
      * \throws boost::system::system_error on failure
      */
-    std::string readLine()
-    {
-        //Reading data char by char, code is optimized for simplicity, not speed
-        using namespace boost;
-        char c;
-        std::string result;
-        for(;;)
-        {
-            asio::read(serial,asio::buffer(&c,1));
-            switch(c)
-            {
-                case '\r':
-                    break;
-                case '\n':
-                    return result;
-                default:
-                    result+=c;
-            }
-        }
-    }
+    std::string readLine();
 
 private:
     boost::asio::io_service io;
---
+++ src/SimpleSerial.cpp
@@ -0,0 +1,43 @@
+/* 
+ * File:   SimpleSerial.cpp
+ * Author: Terraneo Federico wrote SimpleSerial.h, but ericfont copied to this .cpp file
+ * Distributed under the Boost Software License, Version 1.0.
+ *
+ * Created on September 10, 2009, 12:12 PM
+ */
+
+#include <boost/asio.hpp>
+#include "SimpleSerial.h"
+
+SimpleSerial::SimpleSerial(std::string port, unsigned int baud_rate)
+    : io(), serial(io,port)
+    {
+        serial.set_option(boost::asio::serial_port_base::baud_rate(baud_rate));
+    }
+
+void SimpleSerial::writeString(std::string s)
+    {
+        boost::asio::write(serial,boost::asio::buffer(s.c_str(),s.size()));
+    }
+
+std::string SimpleSerial::readLine()
+    {
+        //Reading data char by char, code is optimized for simplicity, not speed
+        using namespace boost;
+        char c;
+        std::string result;
+        for(;;)
+        {
+            asio::read(serial,asio::buffer(&c,1));
+            switch(c)
+            {
+                case '\r':
+                    break;
+                case '\n':
+                    return result;
+                default:
+                    result+=c;
+            }
+        }
+    }
+
